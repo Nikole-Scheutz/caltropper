4979c8eca90026bd9e2807ee01867cb53a00a230
Nikole-Scheutz <nikolai@jinxan.se>
2023-05-14 19:55:00 +0200

    fix imports and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use caltropper::send_key_presses;
-use caltropper::command_parser;
+use caltropper::Caltropper;
 
 fn main() {
-    println!("Hello, world!");
-
-    //command_parser("789,636,789,414.5".to_string());
-    
-    let mut keyboard = caltropper::Command::new("789,636,789,414.5".to_string());
+    let mut caltropper = Caltropper::new("789,636,789,414.5".to_string());
 
     caltropper::wait_ms(5000);
-    keyboard.place_multiple("789".to_string());
+
+    caltropper.place_multiple("789".to_string());
 }
